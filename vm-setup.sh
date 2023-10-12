fba9d8d1bfa640f24602ccbd2c1066b21018775d
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2023-10-12 11:17:07 +0200

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -4,6 +4,8 @@ set -eux -o pipefail
 git config --global user.name "Florian Wilhelm"
 git config --global user.email 2292245+fwilhe@users.noreply.github.com
 
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
 echo "
 
 if command -v nala &> /dev/null; then
